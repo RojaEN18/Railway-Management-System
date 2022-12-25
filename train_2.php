22a7d24c8efcb1017f8858153571316a53b1fd39
RojaEN18 <96619041+RojaEN18@users.noreply.github.com>
2022-12-25 08:44:53 +0530

    Update train_2.php
    changed footer

--- train_2.php
+++ train_2.php
@@ -297,7 +297,7 @@ else if((!isset($_POST['byname'])) && (!isset($_POST['bynum'])))
 			<p class="text-right text-info">  &copy; 2018 Copyright PVT Ltd.</p>	
 			</div>
 			<div style="float:right;">
-			<p class="text-right text-info">	Desinged By : projectworlds</p>
+			<p class="text-right text-info"></p>
 			</div>
 		</div>
 		</footer>	
